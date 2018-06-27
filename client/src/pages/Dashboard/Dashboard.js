8689bdb79cb3b14c9588961a13ef8c1ed8836c2b
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-06-27 16:04:51 -0400

    Update dashboard
    Dashboard: create links to page components.

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import {Container} from "../../components/Grid";
+// import {Container} from "../../components/Grid";
+import { Link } from 'react-router-dom';
 import "./Dashboard.css";
 
 class Dashboard extends Component {
@@ -11,9 +12,29 @@ class Dashboard extends Component {
   
   render() {
     return (  
-        <Container>
-          <img alt="dashboardJPG" src="../images/dashboard.jpg"/>
-        </Container>
+        <div className="row btnHolder">
+            <div className="col">
+              <Link to="/play">
+                <img className="dashImg" alt="play" src="../images/Play.png"/>
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/learn">
+                <img className="dashImg" alt="learn" src="../images/Learn.png"/>
+              </Link>
+            </div>
+            <div className="w-100"></div>
+            <div className="col">
+              <Link to="/stats">
+                <img className="dashImg" alt="stats" src="../images/Stats.png"/>
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/">
+                <img className="dashImg" alt="logout" src="../images/Logout.png"/>
+              </Link>
+            </div>
+        </div>
     );
   }
 }
