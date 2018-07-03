c90b24ca1489f8777551b1e63ee0db2ef2e779bb
Unknown <petercarmines@gmail.com>
2018-07-02 20:26:12 -0400

    adding some stuff to indexDB and mongo

--- routes/api/game.js
+++ routes/api/game.js
@@ -4,6 +4,8 @@ const gameController = require("../../controllers/gameController");
 // Matches with "/api/user"
 router.route("/user")
   .post(gameController.update);
+router.route("/newgame")
+  .post(gameController.newGame);
 
 
 
