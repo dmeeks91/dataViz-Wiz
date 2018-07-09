c4b44c880b6505e4c2936c7a93595d1c7d5a1b8f
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-07-09 19:45:48 -0400

    fix Async problem in Play (this.getStats)

--- client/src/pages/Play3/Play.js
+++ client/src/pages/Play3/Play.js
@@ -204,14 +204,17 @@ class Play extends Component {
           if (!round) return; //exit if no guesses made 
           round.index = index;
           API.saveRound(round)
-             .then(data => console.log(data))
+             .then(() => {
+               API.updateGame({
+                  id: round.gameID,
+                  win: round.playerID,
+                  lose: null
+                }).then(() => {
+                  this.getStats();
+                })
+                  .catch(e => console.log(e));
+             })
              .catch(e => console.log(e));
-          API.updateGame({
-            id: round.gameID,
-            win: round.playerID,
-            lose: null
-          }).then(data => console.log(data))
-            .catch(e => console.log(e));
         });
   };
 
@@ -311,7 +314,7 @@ class Play extends Component {
     toast.success(`Time Up!`);
     clearInterval(this.state.timer);
     this.timeUp();
-    this.getStats();
+    //this.getStats();
   }
 
   startNewGame() { 
